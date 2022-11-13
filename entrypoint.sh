4bc14d7a9734e28a87164eee6d09a221a95913c2
Daniel Silvestre <danielsilvestre.work@gmail.com>
2022-11-14 00:31:39 +0100

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wait-until "mysql -u ${DATABASE_USER} -p${DATABASE_PASSWORD} -h ${DATABASE_HOST} ${DATABASE_NAME} -e 'select 1'"
 cd /app
-composer install
-./yii migrate --interactive=0
+runuser -l www-data -c 'composer install'
+runuser -l www-data -c './yii migrate --interactive=0'
 apache2-foreground
