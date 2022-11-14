8e278ca8bebb870d7d8564afe786df01d72a72da
Daniel Silvestre <danielsilvestre.work@gmail.com>
2022-11-14 01:11:05 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,4 @@ RUN apt-get -yq update && apt-get -yq install default-mysql-client cron
 ADD https://raw.githubusercontent.com/nickjj/wait-until/v0.2.0/wait-until /usr/local/bin
 RUN chmod +x /usr/local/bin/wait-until && chmod +x /app/entrypoint.sh  && chmod +x /app/yii
 
-COPY crontab /etc/crontab
-
-RUN useradd cuptodate --home /home/cuptodate --create-home --groups www-data
-RUN chown -R cuptodate:www-data /app
-
-WORKDIR /app
-
 CMD ["/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 wait-until "mysql -u ${DATABASE_USER} -p${DATABASE_PASSWORD} -h ${DATABASE_HOST} ${DATABASE_NAME} -e 'select 1'"
-cd /app
-runuser -l cuptodate -c 'cd /app && composer install'
-runuser -l cuptodate -c '/app/yii migrate --interactive=0'
+
+cd /app && composer install
+/app/yii migrate --interactive=0
+chown -R www-data:www-data /app
+crontab -r
+(crontab -l 2>/dev/null; echo "* * * * * /app/yii update") | crontab -
 apache2-foreground
