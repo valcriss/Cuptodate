f631f949265bd26b7245c3263bcdc5843e3de897
Daniel Silvestre <danielsilvestre.work@gmail.com>
2022-11-14 12:00:47 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wait-until "mysql -u ${DATABASE_USER} -p${DATABASE_PASSWORD} -h ${DATABASE_HOST} ${DATABASE_NAME} -e 'select 1'"
+wait-until "mysql -u ${DATABASE_USER} -p${DATABASE_PASSWORD} -h ${DATABASE_HOST} ${DATABASE_NAME} -e 'select 1'" 600
 
 cd /app && composer install
 /app/yii migrate --interactive=0
